93fae79a0b1762b294ecd006aa0a92415fa4574f
Bmp83 <bp22malo@siena.edu>
2023-02-18 22:14:27 -0500

    Added the instance variables

--- StockHolding.java
+++ StockHolding.java
@@ -1,16 +1,22 @@
 /**
  * Write a description of class Asset here.
  *
- * @author (your name)
+ * @author Brian Maloney
  * @version (a version number or a date)
  */
 public class StockHolding
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+    public int symbol;
+    public String name;
+    public int numShares;
+    public int price;
+    
+    
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
+        symbol, name, numShares, price, numShares * price); 
+    }
 }
