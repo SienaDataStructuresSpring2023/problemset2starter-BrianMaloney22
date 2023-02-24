d7794eef1adf997f34cf417757ad07a8f205b540
Bmp83 <bp22malo@siena.edu>
2023-02-24 10:18:45 -0500

    Complete the banner comment and the method comments

--- StockHolding.java
+++ StockHolding.java
@@ -6,39 +6,74 @@
  */
 public class StockHolding
 {
-    public String symbol;
-    public String name;
-    public int numShares;
-    public Double price;
-    
-    public StockHolding(String sym, String names, int share, Double prices){
-        symbol = sym;
-        name = names;
-        numShares = share;
-        price = prices;
+    private String symbol;
+    private String name;
+    private int numShares;
+    private Double price;
+
+    public StockHolding(String symbol, String name, int numShares, Double price){
+        this.symbol = symbol;
+        this.name = name;
+        this.numShares = numShares;
+        this.price = price;
     }
     
+    /**
+    * Returns the stock symbol.
+    *
+    * @return The stock symbol.
+    */
     public String getSymbol(){
         return symbol;
     }
     
+    /**
+    * Returns the stock name.
+    *
+    * @return The stock name.
+    */
     public String getName(){
         return name;
     }
     
+    /**
+    * Returns the number of shares in the stock.
+    *
+    * @return the number of shares in the stock.
+    */
     public int getShares(){
         return numShares;
     }
     
+    /**
+    * Returns the price of the stock.
+    *
+    * @return The price of the stock.
+    */
     public Double getPrice(){
         return price;
     }
     
-    public void byShares(int buy, double prices){
+    /**
+     * Takes the input buy and prices and assigns them to numShares and price.
+     * 
+     * @param buy the number of shares to buy.
+     * @param the price per share.
+     */
+    public void buyShares(int buy, double prices){
         numShares = buy;
         price = prices;
     }
-    
+
+    /**
+     * Takes input the number of shares to sell and 
+     * if there is at least as many sell as there are numShares, it reurns 
+     * the sell times price.
+     * 
+     * @param sell the number of shares to sell.
+     * 
+     * @return the dollar amount resulting from the sale of the shares.
+     */
     public double sellShares(int sell){
         double money = 0;
         if(numShares >= sell){
@@ -46,12 +81,18 @@ public class StockHolding
         }
         return money;
     }
-    
+
+    /**
+     * Returns a string containing the stock symbol, name, number of shares, price,
+     * and the number of shares times the price.
+     * 
+     * @return A string describing the state of the object.
+     */
     @Override
     public String toString()
     {
         //DO NOT EDIT THIS METHOD.
         return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        symbol, name, numShares, price, numShares * price); 
+            symbol, name, numShares, price, numShares * price); 
     }
 }
