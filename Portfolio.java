cd6031be055e1bbbf543ee5468fbc8ea01a2c906
Bmp83 <bp22malo@siena.edu>
2023-02-24 09:35:56 -0500

    fixed all the errors

--- Portfolio.java
+++ Portfolio.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 
 /**
  * Write a description of class Portfolio here.
@@ -10,61 +10,62 @@ import java.util.List;
 
 public class Portfolio
 {
-    private StockHolding [] stocks;
-    private int investment;
-    private int payout;
+    private ArrayList<StockHolding> stocks = new ArrayList<StockHolding>();
+    private double investment;
+    private double payout;
 
     public Portfolio(){
         investment = 0;
         payout = 0;
-        stocks = new StockHolding [5];
+        stocks = new ArrayList <> ();
     }
-    
-    public int getPayout(){
+
+    public double getPayout(){
         return payout;
     }
 
-    public int getInvestment(){
+    public double getInvestment(){
         return investment;
     }
-    
+
     private int getIndex(String symbol){
-        for(int i = 0; i < stocks.length; i++){
-            if(stocks[i].getSymbol().equals(symbol)){
-              return i;
+        for(int i = 0; i < stocks.size(); i++){
+            if(stocks.get(i).getSymbol().equals(symbol)){
+                return i;
             }
         }
         return -1;
     }
-    
+
     public double buyStock(String symbol, String name, int buy, double price){
         if(getIndex(symbol) != -1){
-            stocks[getIndex(symbol)].buyShares(buy, price);
+            stocks.get(getIndex(symbol)).buyShares(buy, price);
         } else {
-            //stocks.add(symbol);
+            stocks.add(new StockHolding(symbol, name, buy, price));
         }
+        investment += buy * price;
         return (buy * price);
     }
-    
+
     public double sellStock(String symbol, int sell){
         if(getIndex(symbol) != -1){
-            if(stocks[getIndex(symbol)].sellShares(sell) == 0){
-                stocks[getIndex(symbol)] = null;;
+            if(stocks.get(getIndex(symbol)).sellShares(sell) == 0){
+                stocks.remove(getIndex(symbol));
             } else {
-                stocks[getIndex(symbol)].sellShares(sell);
+                stocks.get(getIndex(symbol)).sellShares(sell);
             }
         }
-        return stocks[getIndex(symbol)].sellShares(sell);
+        return stocks.get(getIndex(symbol)).sellShares(sell);
     }
-    
-    public int getCurrentValue(){
-        int total = 0;
-        for(int i = 0; i < stocks.length; i++){
-            total += stocks[i].getPrice() * stocks[i].getShares();
+
+    public double getCurrentValue(){
+        double total = 0;
+        for(int i = 0; i < stocks.size(); i++){
+            total += stocks.get(i).getPrice() * stocks.get(i).getShares();
         }
         return total;
     }
-    
+
     @Override
     public String toString()
     {
