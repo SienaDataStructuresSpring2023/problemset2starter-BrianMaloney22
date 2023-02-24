a6c447747987cb2ab723a837d24a05fb74489750
Bmp83 <bp22malo@siena.edu>
2023-02-23 23:45:17 -0500

    added the getIndex method

--- Portfolio.java
+++ Portfolio.java
@@ -9,27 +9,45 @@ import java.util.ArrayList;
 
 public class Portfolio
 {
-    private ArrayList [] StockHolding;
+    private StockHolding [] stocks;
     private int investment;
     private int payout;
-    
+
     public Portfolio(){
         investment = 0;
         payout = 0;
-        StockHolding = new ArrayList [5];
+        stocks = new StockHolding [5];
     }
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // StringBuffer sb = new StringBuffer();
-        // sb.append(String.format("%6s%25s%10s%15s%15s%n", 
-                // "Symbol", "Name", "Shares", "@Price", "Total"));
-        // sb.append("-----------------------------------------------------------------------\n");
+    
+    public int getPayout(){
+        return payout;
+    }
 
-        // for(StockHolding s : stocks){
-            // sb.append(s.toString());
-        // }
-        // return sb.toString();
-    // }
+    public int getInvestment(){
+        return investment;
+    }
+    
+    private int getIndex(String symbol){
+        for(int i = 0; i < stocks.length; i++){
+            if(stocks[i].getSymbol().equals(symbol)){
+              return i;
+            }
+        }
+        return -1;
+    }
+    
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("%6s%25s%10s%15s%15s%n", 
+                "Symbol", "Name", "Shares", "@Price", "Total"));
+        sb.append("-----------------------------------------------------------------------\n");
+
+        for(StockHolding s : stocks){
+            sb.append(s.toString());
+        }
+        return sb.toString();
+    }
 }
